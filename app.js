582b49e131a9301c17a662cf439c405f721cb764
AndyLampert <lamperta@gmail.com>
2014-05-06 15:49:09 -0600

    added blocking method (synchronous using readFileSync)

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
+// including filesystem core module
+var fs = require('fs');
+var fileContents = fs.readFileSync('text.txt');
+
 var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-	res.send('<h1>Hello Boulder</h1>');
+	res.header('Content-Type', 'text/plain');
+	res.send(fileContents);
 });
 
-var server = app.listen(3384, function() {
+var server = app.listen(3021, function() {
 	console.log('Express server listening on port ' + server.address().port);
 });
